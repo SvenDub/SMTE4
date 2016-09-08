a50e61fde239c568c946d504c962222a56e41e4e
Sven Dubbeld <sven.dubbeld1@gmail.com>
2016-09-08 11:24:38 +0200

    Add ReportActivity

--- app/src/main/java/nl/svendubbeld/smte4/MainActivity.kt
+++ app/src/main/java/nl/svendubbeld/smte4/MainActivity.kt
@@ -1,12 +1,18 @@
 package nl.svendubbeld.smte4
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_report.setOnClickListener {
+            startActivity(Intent(applicationContext, ReportActivity::class.java))
+        }
     }
 }
